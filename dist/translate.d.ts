f66f11d2a0e28be69047e5f170cf53820fc3ffd7
huangjiaqi <lilsnake239299@gmail.com>
2025-03-03 16:55:27 +0800

    update build

--- dist/translate.d.ts
+++
@@ -1,2 +0,0 @@
-import type { TranslateConfig, StringObject, ILocalsOption } from '../types';
-export default function (localePath: string, locales: ILocalsOption[], oldPrimaryLang: StringObject, options: TranslateConfig): Promise<void>;
